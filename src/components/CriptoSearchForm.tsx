eaa9b0228b0b654a09c8e004fdbb53f49c64c9f0
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-11-27 22:01:53 -0400

    leyendo nuestro state y mostrandolo en la UI

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,12 +1,20 @@
 import { currencies } from "../data";
+import { useCryptoStore } from "../stores/crypto-store";
 
 export default function CriptoSearchForm() {
+  const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies);
+
   return (
     <form className="form">
       <div className="field">
         <label htmlFor="currency">Moneda</label>
         <select name="currency" id="currency">
           <option value="">--- Seleccione ---</option>
+          {cryptocurrencies.map((crypto) => (
+            <option key={crypto.CoinInfo.FullName} value={crypto.CoinInfo.Name}>
+              {crypto.CoinInfo.FullName}
+            </option>
+          ))}
         </select>
       </div>
 
