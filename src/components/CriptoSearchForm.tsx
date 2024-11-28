1923273cd92acf2b461a7f6fd0c64885eead9806
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-11-27 22:10:06 -0400

    creado state local para manejar el formulario de criptos

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,14 +1,27 @@
+import { ChangeEvent, useState } from "react";
 import { currencies } from "../data";
 import { useCryptoStore } from "../stores/crypto-store";
+import { Pair } from "../types";
 
 export default function CriptoSearchForm() {
   const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies);
+  const [pair, setPair] = useState<Pair>({
+    currency: "",
+    criptocurrency: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setPair({
+      ...pair,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <form className="form">
       <div className="field">
         <label htmlFor="currency">Moneda</label>
-        <select name="currency" id="currency">
+        <select name="currency" id="currency" onChange={handleChange}>
           <option value="">--- Seleccione ---</option>
           {cryptocurrencies.map((crypto) => (
             <option key={crypto.CoinInfo.FullName} value={crypto.CoinInfo.Name}>
@@ -20,7 +33,7 @@ export default function CriptoSearchForm() {
 
       <div className="field">
         <label htmlFor="criptocurrency">Criptomoneda</label>
-        <select name="criptocurrency" id="criptocurrency">
+        <select name="criptocurrency" id="criptocurrency" onChange={handleChange}>
           <option value="">--- Seleccione ---</option>
           {currencies.map((currency) => (
             <option key={currency.name} value={currency.code}>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
-import { CryptoCurrencyResponseSchema, CurrencySchema } from "../schemas/crypto-schema";
+import { CryptoCurrencyResponseSchema, CurrencySchema, PairSchema } from "../schemas/crypto-schema";
 
 export type Currency = z.infer<typeof CurrencySchema>;
 export type Cryptocurrency = z.infer<typeof CryptoCurrencyResponseSchema>;
+
+export type Pair = z.infer<typeof PairSchema>;
