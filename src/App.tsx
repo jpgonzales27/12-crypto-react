8d8144d07e871736e2380e73d01a586a453a5aa0
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-11-28 22:25:20 -0400

    mostando resultado en el display de crypto price

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import CriptoSearchForm from "./components/CriptoSearchForm";
 import { useCryptoStore } from "./stores/crypto-store";
+import CryptoPriceDisplay from "./components/CryptoPriceDisplay";
 
 function App() {
   const fetchCrypto = useCryptoStore((state) => state.fetchCryptos);
@@ -17,6 +18,7 @@ function App() {
         </h1>
         <div className="content">
           <CriptoSearchForm />
+          <CryptoPriceDisplay />
         </div>
       </div>
     </>
